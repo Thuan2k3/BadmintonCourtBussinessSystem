c6db9c312d4ed3014ae688ee7548c3e3cbbc03ec
THUAN\tranm <thuant1002@gmail.com>
2025-03-14 14:40:58 +0700

    Tru uy tin neu khach hang khong den 2

--- models/timeSlotBookingModel.js
+++ models/timeSlotBookingModel.js
@@ -8,7 +8,7 @@ const timeSlotBookingSchema = new mongoose.Schema({
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
+    ref: "customers",
     required: true,
   },
   court: {
