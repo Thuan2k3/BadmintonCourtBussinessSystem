be742d7120c6c23b952c9c8353f14cb46aaf8c61
THUAN\tranm <thuant1002@gmail.com>
2025-04-06 11:47:24 +0700

    phan quyen truy cap trang tren frontend

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import axios from "axios";
 
 import { useSelector, useDispatch } from "react-redux";
 import { hideLoading, showLoading } from "../redux/features/alertSlice";
 import { setUser } from "../redux/features/userSlice";
 
+import {
+  adminMenu,
+  employeeMenu,
+  customerMenu,
+  guestMenu,
+} from "../data/data"; // import các menu
+
 export const ProtectedRoute = ({ children }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.user);
+  const location = useLocation();
+
+  // Lấy đường dẫn hiện tại
+  const currentPath = location.pathname;
 
   //get user
   //eslint-disable-next-line
@@ -44,9 +55,41 @@ export const ProtectedRoute = ({ children }) => {
     }
   }, [user, getUser]);
 
-  if (localStorage.getItem("token")) {
-    return children;
-  } else {
+  // Nếu chưa có token thì không cho vào
+  if (!localStorage.getItem("token")) {
     return <Navigate to="/home" />;
   }
+
+  // Nếu đã có user, kiểm tra role và quyền truy cập
+  if (user) {
+    // Xác định danh sách menu theo vai trò
+    let allowedMenu = [];
+
+    switch (user.role) {
+      case "admin":
+        allowedMenu = adminMenu;
+        break;
+      case "employee":
+        allowedMenu = employeeMenu;
+        break;
+      case "customer":
+        allowedMenu = customerMenu;
+        break;
+      default:
+        allowedMenu = guestMenu;
+    }
+
+    // Lấy danh sách path từ menu
+    const allowedPaths = allowedMenu.map((item) => item.path);
+
+    // Nếu currentPath nằm trong allowedPaths thì cho truy cập
+    if (allowedPaths.includes(currentPath)) {
+      return children;
+    } else {
+      return <Navigate to="/" />;
+    }
+  }
+
+  // Trường hợp user chưa được load xong
+  return null;
 };
